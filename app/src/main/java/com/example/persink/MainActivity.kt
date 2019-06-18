39658b7889c3e158a62d6e28347617a9ef79be7d
Kadhana <rere_khinsider@yahoo.com>
2019-06-18 20:59:57 +0700

    Update 2

--- app/src/main/java/com/example/persink/MainActivity.kt
+++ app/src/main/java/com/example/persink/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : AppCompatActivity() {
 
         createNotification10Button.setOnClickListener {
             // Starts the function below
-            addNotificationTimed("10")
+            addNotificationTimed("100")
         }
         createSTNKButton.setOnClickListener {
             // Starts the function below
