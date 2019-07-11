9ee24e67381cb4c4aa93d8bc071d66b16c954806
Kadhana <rere_khinsider@yahoo.com>
2019-07-12 00:03:53 +0700

    Added View STNK / SIM (Not Tested)

--- app/src/main/java/com/example/persink/lihatsim.kt
+++ app/src/main/java/com/example/persink/lihatsim.kt
@@ -2,11 +2,55 @@ package com.example.persink
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_lihatsim.*
 
 class lihatsim : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lihatsim)
+        showDataSIM()
+    }
+
+    fun showDataSIM(){
+        tvDisplayNameSIM.text = ""
+        val dbHandler = DatabaseHelperSIM(this, null)
+        val cursor = dbHandler.getAll()
+        cursor!!.moveToFirst()
+        tvDisplayNameSIM.append((cursor.getString(0)))
+        tvDisplayNameSIM.append("    ")
+        tvDisplayNameSIM.append((cursor.getString(1)))
+        tvDisplayNameSIM.append("    ")
+        tvDisplayNameSIM.append((cursor.getString(2)))
+        tvDisplayNameSIM.append("    ")
+        tvDisplayNameSIM.append((cursor.getString(3)))
+        tvDisplayNameSIM.append("    ")
+        tvDisplayNameSIM.append((cursor.getString(4)))
+        tvDisplayNameSIM.append("    ")
+        tvDisplayNameSIM.append((cursor.getString(5)))
+        tvDisplayNameSIM.append("    ")
+        tvDisplayNameSIM.append((cursor.getString(6)))
+        tvDisplayNameSIM.append("    ")
+        tvDisplayNameSIM.append((cursor.getString(7)))
+        tvDisplayNameSIM.append("\n")
+        while (cursor.moveToNext()) {
+            tvDisplayNameSIM.append((cursor.getString(0)))
+            tvDisplayNameSIM.append("    ")
+            tvDisplayNameSIM.append((cursor.getString(1)))
+            tvDisplayNameSIM.append("    ")
+            tvDisplayNameSIM.append((cursor.getString(2)))
+            tvDisplayNameSIM.append("    ")
+            tvDisplayNameSIM.append((cursor.getString(3)))
+            tvDisplayNameSIM.append("    ")
+            tvDisplayNameSIM.append((cursor.getString(4)))
+            tvDisplayNameSIM.append("    ")
+            tvDisplayNameSIM.append((cursor.getString(5)))
+            tvDisplayNameSIM.append("    ")
+            tvDisplayNameSIM.append((cursor.getString(6)))
+            tvDisplayNameSIM.append("    ")
+            tvDisplayNameSIM.append((cursor.getString(7)))
+            tvDisplayNameSIM.append("\n")
+        }
+        cursor.close()
     }
 }
--- app/src/main/java/com/example/persink/lihatstnk.kt
+++ app/src/main/java/com/example/persink/lihatstnk.kt
@@ -2,11 +2,55 @@ package com.example.persink
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_lihatstnk.*
 
 class lihatstnk : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lihatstnk)
+        showDataSTNK()
+    }
+
+    fun showDataSTNK(){
+        tvDisplayNameSTNK.text = ""
+        val dbHandler = DatabaseHelperSTNK(this, null)
+        val cursor = dbHandler.getAll()
+        cursor!!.moveToFirst()
+        tvDisplayNameSTNK.append((cursor.getString(0)))
+        tvDisplayNameSTNK.append("    ")
+        tvDisplayNameSTNK.append((cursor.getString(1)))
+        tvDisplayNameSTNK.append("    ")
+        tvDisplayNameSTNK.append((cursor.getString(2)))
+        tvDisplayNameSTNK.append("    ")
+        tvDisplayNameSTNK.append((cursor.getString(3)))
+        tvDisplayNameSTNK.append("    ")
+        tvDisplayNameSTNK.append((cursor.getString(4)))
+        tvDisplayNameSTNK.append("    ")
+        tvDisplayNameSTNK.append((cursor.getString(5)))
+        tvDisplayNameSTNK.append("    ")
+        tvDisplayNameSTNK.append((cursor.getString(6)))
+        tvDisplayNameSTNK.append("    ")
+        tvDisplayNameSTNK.append((cursor.getString(7)))
+        tvDisplayNameSTNK.append("\n")
+        while (cursor.moveToNext()) {
+            tvDisplayNameSTNK.append((cursor.getString(0)))
+            tvDisplayNameSTNK.append("    ")
+            tvDisplayNameSTNK.append((cursor.getString(1)))
+            tvDisplayNameSTNK.append("    ")
+            tvDisplayNameSTNK.append((cursor.getString(2)))
+            tvDisplayNameSTNK.append("    ")
+            tvDisplayNameSTNK.append((cursor.getString(3)))
+            tvDisplayNameSTNK.append("    ")
+            tvDisplayNameSTNK.append((cursor.getString(4)))
+            tvDisplayNameSTNK.append("    ")
+            tvDisplayNameSTNK.append((cursor.getString(5)))
+            tvDisplayNameSTNK.append("    ")
+            tvDisplayNameSTNK.append((cursor.getString(6)))
+            tvDisplayNameSTNK.append("    ")
+            tvDisplayNameSTNK.append((cursor.getString(7)))
+            tvDisplayNameSTNK.append("\n")
+        }
+        cursor.close()
     }
 }
